d5271c987a6330b3e57a3a22ea283b5169b55159
Phuoc Vo <130758054+phuocnehihi1@users.noreply.github.com>
2024-12-10 22:21:22 +0700

    Search page (#2)
    * Search-page
    The-Fist-Commit-Code
    
    * The Fist Commit Code

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "url";
 import path from "path";
 
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
